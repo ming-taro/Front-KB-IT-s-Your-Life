f6d3d909dc24df3c6781ef1b969d6f3bc152705e
ming-taro <hera002008@naver.com>
2024-05-27 16:24:14 +0900

    feat: vue 프로젝트 생성 및 컴포넌트 생성

---
+++ work_space_front_vue_kb5/5.vue_sfc_basic/vue-sfc-1-app/src/components/CheckBoxItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- 컴포넌트의 기본 HTML을 작성하는 영역 -->
+    <li>
+        <input type="checkbox" v-model="checked">
+        <span class="test">옵션1</span>
+    </li>
+
+</template>
+
+<script>
+// script 오는 곳
+export default {
+    name: 'CheckBoxItem',
+    data() {
+        return {
+            checked: false,
+        };
+    }
+}
+</script>
+
+<style scoped>
+    /* style scoped */
+    /* css를 적용 가능한 영역 */
+    /* 가능하면 사용하지 말고, css 파일에 정의하는 것이 일반적 */
+    .test {
+        color: blue;
+    }
+</style>
