83ab0456c14fa4fba05779fc452b51380c77c07e
ming-taro <hera002008@naver.com>
2024-05-29 14:00:16 +0900

    feat: 체크박스 컴포넌트

---
+++ work_space_front_vue_kb5/6.vue_sfc_in_depth/vue-sfc-1-css-issue/src/components/Child5.vue
@@ -0,0 +1,13 @@
+<template>
+    <div :class="$style.child">
+        <h2>Child5 - main.css에 child5로 별도로 선언한 영역</h2>
+    </div>
+</template>
+<script>
+export default {
+    name: "Child5",
+    created() {
+        console.log(this.$style);
+    },
+}
+</script>
---
+++ work_space_front_vue_kb5/6.vue_sfc_in_depth/vue-sfc-2-slot/src/components/CheckBox2.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <input type="checkbox" :value="id" :checked="checked"
+            @change="$emit('check-changed', { id, checked: $event.target.checked })">
+        <!-- 외부에서 주입된 slot이 들어갈 자리 -->
+        <slot>Item</slot>
+    </div>
+</template>
+<script>
+export default {
+    name: 'CheckBox2',
+    props: ['id', 'checked'] // 'label'없이
+}
+</script>
