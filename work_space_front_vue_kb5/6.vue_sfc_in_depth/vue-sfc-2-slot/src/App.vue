abab427024b5fe765429ba136969e91bcce42b95
ming-taro <hera002008@naver.com>
2024-05-29 14:21:32 +0900

    fix: 화면 오류 수정중

--- work_space_front_vue_kb5/6.vue_sfc_in_depth/vue-sfc-2-slot/src/App.vue
+++ work_space_front_vue_kb5/6.vue_sfc_in_depth/vue-sfc-2-slot/src/App.vue
@@ -19,7 +19,7 @@
 import NoSlotTest from './components/NoSlotTest.vue'
 import SlotTest from './components/SlotTest.vue'
 import NamedSlotTest from './components/NamedSlotTest.vue'
-import ScopedSlotTest from './components/NamedSlotTest.vue'
+import ScopedSlotTest from './components/ScopedSlotTest.vue'
 
 export default {
   name: "App",
---
+++ work_space_front_vue_kb5/6.vue_sfc_in_depth/vue-sfc-2-slot/src/components/Layout.vue
@@ -0,0 +1,77 @@
+<template>
+    <Layout>
+        <!-- 사용자가 정의한 slot이 작성되는 영역 -->
+        <template v-slot:header>
+        </template>
+        <template v-slot:sidebar>
+        </template>
+        <template v-slot:default>
+        </template>
+        <template v-slot:footer>
+        </template>
+    </Layout>
+
+    <div class="wrapper">
+        <header class="box header">
+            <slot name="header"></slot>
+        </header>
+        <aside class="box sidebar">
+            <slot name="sidebar"></slot>
+        </aside>
+        <section class="box content">
+            <!-- default 자리 -->
+            <slot></slot>
+        </section>
+        <footer class="box footer">
+            <slot name="footer"></slot>
+        </footer>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+body {
+    margin: 40px;
+}
+
+.sidebar {
+    grid-area: sidebar;
+}
+
+.content {
+    grid-area: content;
+    position: relative;
+}
+
+.header {
+    grid-area: header;
+}
+
+.footer {
+    grid-area: footer;
+}
+
+.wrapper {
+    display: grid;
+    grid-gap: 5px;
+    grid-template-rows: 100px 1fr 100px;
+    grid-template-columns: 1fr 200px;
+    grid-template-areas:
+        "header  header"
+        "content sidebar"
+        "footer  footer";
+    background-color: #fff;
+    color: #444;
+}
+
+.box {
+    background-color: #ddd;
+    color: #444;
+    border-radius: 5px;
+    padding: 10px;
+    font-size: 100%;
+}
+</style>
