0ffd56a7749970a2a3ce7c69161ee38fa97e80fa
ming-taro <hera002008@naver.com>
2024-05-31 15:13:47 +0900

    feat: 가수 소개 페이지 만들기

---
+++ work_space_front_vue_kb5/8.vue_route/vue_router_1_basic/src/pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card card-body">
+        <h2>About</h2>
+        <img src="https://i.namu.wiki/i/UHJqvB6CPT8IZFPIaj6ypU5siIJnixwhIdbepCeSbYw0S-dDvjBNA4pobS3Jvxi0_7O990-ItEg2eElaVhe2673iUui1TkfFtFERXkVCBSj3kl7rs-ONPEhp2q_popI-dHkWAujCxgVzUJWi4OrEuw.webp" alt="">
+        <p>요청 경로 : {{  $route.fullPath }}</p>
+    </div>
+</template>
+
+<script setup>
+// export default {
+//     name: 'About',
+//     created() {
+//         console.log(this.$route);
+//     },
+// }
+</script>
